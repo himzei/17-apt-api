ac0bdab30232dac11aca22adf07b24b30cb3556f
himzei <himzei@gmail.com>
2026-07-15 13:43:44 +0900

    0715 13:40

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from scrapper import fetch_apt_trade
 
 app = Flask(__name__)
 app.config["TEMPLATES_AUTO_RELOAD"] = True
@@ -6,6 +7,13 @@ app.config["TEMPLATES_AUTO_RELOAD"] = True
 @app.route('/')
 def index():
     return render_template("index.html")
+
+@app.route("/search")
+def search():
+    DEAL_YMD = request.args.get("DEAL_YMD")
+    LAWD_CD = request.args.get("LAWD_CD")
+    results = fetch_apt_trade(LAWD_CD, DEAL_YMD)
+    return render_template("search.html", results=results)
 
 if __name__ == '__main__':
     app.run(debug=True)
